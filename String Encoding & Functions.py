60a55347c06f7d1c969f2fc1043769ff894871a2
Candace Hollinger <csig1985@gmail.com>
2022-12-12 22:57:45 +0000

    updating string encoding function

---
+++ String Encoding & Functions.py
@@ -0,0 +1,15 @@
+#Encoding Types
+ord('a')
+97
+
+#Hexidecimal notation in UTF
+0X2122
+8482
+
+#Using a string using unicode
+ord('\u2122')
+8482
+
+#Using the chr function going back to a string
+chr(8482)
+'TM'
